1639d9e572a86a002873a58f430c461d7c294eb6
aditya gupta <adityagupta93999@gmail.com>
2018-03-31 17:32:07 +0530

    Modified projects

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class project(models.Model):
+    title = models.CharField(max_length=30)
+    summary = models.CharField(max_length=250)
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import project
 # Create your views here.
+
+def home(request):
+    projects = project.objects
+    return render(request, 'projects/home.html', {'projects':projects})
