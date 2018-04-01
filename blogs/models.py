ee455f4d6f7eb3d2fb9c7dcc90db969c1cff532d
aditya gupta <adityagupta93999@gmail.com>
2018-04-01 22:56:33 +0530

    final commit

--- blogs/models.py
+++ blogs/models.py
@@ -8,3 +8,12 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def summary(self):
+        return self.body[:60]
+
+    def display_date(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
+
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import projects.views
@@ -8,4 +8,5 @@ import projects.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', projects.views.home, name='home'),
+    path('blog/', include('blogs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
